43f6d087f45582b67c8320c4e09317ae0ae6caff
Gowtham <gowtham@alagzoo.com>
2020-08-14 23:36:41 +0530

    first commit cli

---
+++ CartApp/App.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import {  } from 'react-native-gesture-handler'
+import React from 'react';
+import {View} from 'react-native';
+import AppNavigation from './src/navigation/index'
+import { Provider } from 'react-redux';
+import store from './src/store/configStore';
+
+const App = ()=>{ 
+  return (
+    <Provider store = {store}>
+      <View style = {{flex:1}}>
+        <AppNavigation/>
+      </View>
+    </Provider>
+  )
+}
+export default App;
---
+++ CartApp/src/types.js
@@ -0,0 +1,4 @@
+export default {
+  ADD_ITEM: 'ADD_ITEM',
+  SET_DATA: 'SET_DATA'
+}
